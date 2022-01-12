4a450a2567ae72d3e710050e45fd70078e6039f9
blueballpen12 <blueballpen12@gmail.com>
2022-01-12 23:47:49 +0900

    Test 다시

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("-------");
+        System.out.println("가나다라마바사 아자차타파하");
+
     }
 }
